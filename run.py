1ccba804ae15889e83a782a0d10c3fd76a159b93
BuStudios <1bustudios@gmail.com>
2024-04-13 23:57:27 +0200

    ✨ status and profile picture changer

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import stashconnect
 from dotenv import load_dotenv
+import datetime
 import os
 
 load_dotenv("config/.env")
@@ -9,5 +10,13 @@ password = os.getenv("password")
 encryption_password = os.getenv("pass2")
 target = os.getenv("conversation_id")
 
-user = stashconnect.Login(email=email, password=password, encryption_password=encryption_password)
-user.send_message(target=target, text="HEllo")
+user = stashconnect.Login(email=email, password=password, 
+                          #encryption_password=encryption_password
+                          )
+
+#while True:
+#    text = input("Input:\n")
+#    user.send_message(target=target, text=text)
+
+user.change_status(str(datetime.datetime.now())[:19])
+user.change_profile_picture(url="https://assets-global.website-files.com/6009ec8cda7f305645c9d91b/620bd6d655f2044afa28bff4_glassmorphism.jpeg")
--- stashconnect/login.py
+++ stashconnect/login.py
@@ -14,6 +14,8 @@ import requests
 import json
 import base64
 
+from PIL import Image
+import io
 
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
@@ -98,7 +100,7 @@ class Login:
         return payload
     
 
-    def get_private_key(self, *, encryption_password):
+    def get_private_key(self, *, encryption_password:str):
 
         print("Importing private key. Please wait...")
 
@@ -127,7 +129,7 @@ class Login:
             return self.conversation_keys[target]
         
 
-    def send_message(self, target, text):
+    def send_message(self, target, text:str):
 
         iv = Crypto.Random.get_random_bytes(16)
         conversation_key = self.get_conversation_key(target=target)
@@ -152,3 +154,40 @@ class Login:
         }
 
         self._post("message/send", data=data)
+    
+
+    def change_status(self, status):
+
+        response = self._post("account/change_status", data={"status": status})
+        return response
+    
+
+    def change_profile_picture(self, *, url):
+        
+        response = requests.get(url)
+        response.raise_for_status()
+        
+
+        with Image.open(io.BytesIO(response.content)) as image:
+
+            min_dimension = min(image.width, image.height)
+
+            scale_factor = 512 / min_dimension
+
+            new_width = int(image.width * scale_factor)
+            new_height = int(image.height * scale_factor)
+
+            image = image.resize((new_width, new_height), Image.Resampling.LANCZOS)
+
+            left, top = (new_width - 512) / 2, (new_height - 512) / 2
+            right, bottom = left + 512, top + 512
+
+            image = image.crop((left, top, right, bottom))
+
+            buffered = io.BytesIO()
+            image.save(buffered, format="PNG")
+
+            image_base64 = base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+            response = self._post("account/store_profile_image", data={"imgBase64": f"data:image/png;base64,{image_base64}"})
+            return response
