095e086973fd8fc63a0402b4b217b32247acdf90
BuStudios <1bustudios@gmail.com>
2024-05-10 21:00:51 +0200

    📚 docstrings

--- stashconnect/companies.py
+++ stashconnect/companies.py
@@ -2,13 +2,26 @@ from .models import Company
 
 
 class CompanyManager:
-    def __init__(self, client):
+    def __init__(self, client) -> None:
         self.client = client
 
-    def info(self, company_id: str | int):
+    def info(self, company_id: str | int) -> Company:
+        """Gets the info of a company
+
+        Args:
+            company_id (str | int): The companies id.
+
+        Returns:
+            Company: A company object.
+        """
         response = self.client._post("company/details", data={"company_id": company_id})
         return Company(self.client, response["company"])
 
-    def member(self):
+    def member(self) -> list:
+        """Lists the companies of the logged in user
+
+        Returns:
+            list: The company objects in a list.
+        """
         response = self.client._post("company/member", data={"no_cache": True})
         return [Company(self.client, data) for data in response["companies"]]
