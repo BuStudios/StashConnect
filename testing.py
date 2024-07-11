da96b5b64eb5c8e5c15008b25e08ab1413bf37a1
BuStudios <1bustudios@gmail.com>
2024-07-11 12:00:00 +0000

    clean testing code

--- testing.py
+++ testing.py
@@ -1,9 +1,6 @@
 import stashconnect
 from dotenv import load_dotenv
-import sys
 import os
-import pprint
-import time
 
 load_dotenv("config/.env")
 
@@ -11,43 +8,14 @@ email = os.getenv("email")
 password = os.getenv("password")
 encryption_password = os.getenv("pass2")
 target = os.getenv("conversation_id")
-target_channel = os.getenv("channel_id")
-company_id = os.getenv("company")
 
 client = stashconnect.Client(
-    email=email, password=password,
-    #encryption_password=encryption_password,
+    email=email,
+    password=password,
+    # encryption_password=encryption_password,
     device_id="99dme98fsefmf8fuscpdu",
     app_name="maintest",
 )
-
-file = client.files.upload(target, "setup.py", encrypted=False)
-print(file.download_bytes())
-
-# print(client.auth.verify_login())
-sys.exit()
-
-# Upload to the personal folder:
-# print(client.files.upload(client.user_id, "setup.py", None))
-
-file = client.files.upload(target, "LICENSE", False)
-print(file.name)
-client.files.store_preview_image(file.id, "assets/icon-3.png")
-sys.exit()
-
-# members = client.channels.members(target_channel)
-# for member in members:
-#    print(member.first_name)
-
-print(client.companies.list_features(company_id))
-
-"""
-messages = client.messages.get_flagged(target)
-for message in messages:
-    print(message.content)
-    message.unflag()
-sys.exit()
-"""
 
 
 @client.event("notification")
@@ -68,11 +36,6 @@ def message_received(data):
 @client.event("user-started-typing")
 def user_typing(data):
     print("User writing: " + str(data))
-
-
-@client.loop(seconds=3000)
-def loop():
-    pass
 
 
 client.run()
